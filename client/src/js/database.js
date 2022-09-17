fae4bd497422d64ee542c9d633cd392eedd9a986
Joshua Rae <joshuar2155@gmail.com>
2022-09-16 23:12:43 -0400

    updates

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,19 +12,39 @@ const initdb = async () =>
     },
   });
 
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => 
+{
+ 
+  const jateDB = await openDB('jate', 1);
+    
+	const tx = jateDB.transaction('jate', 'readwrite');
+  
+	const store = tx.objectStore('jate');
+
+	const request = store.put({ id: 1, value: content });
+	const result = await request;
+	console.log(result);
+}
+
 
 export const getDb = async () => 
 {
   console.log('GET from the database');
- 
+
   const jateDb = await openDB('jate', 1);
- 
+
+
   const tx = jateDb.transaction('jate', 'readonly');
 
   const store = tx.objectStore('jate');
 
-  const request = store.getAll();
+
+  const request = store.get(1);
+
+  const result = await request;
+
+  return result?.value;
+ 
 }
 
 initdb();
